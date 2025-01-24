a3b14fdd251e70df1f7c7a90d38a050fdfbaacf2
vinay-samespace <vinay.khanagavi@samespace.com>
2025-01-24 16:03:07 +0530

    Changes to be committed: 	new file:   .gitignore 	new file:   .npmrc 	new file:   .prettierignore 	new file:   .prettierrc 	new file:   components.json 	new file:   eslint.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   src/app.css 	new file:   src/app.d.ts 	new file:   src/app.html 	new file:   src/lib/components/ui/button/button.svelte 	new file:   src/lib/components/ui/button/index.ts 	new file:   src/lib/index.ts 	new file:   src/lib/utils.ts 	new file:   src/routes/+layout.svelte 	new file:   src/routes/+page.svelte 	new file:   static/favicon.png 	new file:   svelte.config.js 	new file:   tailwind.config.ts 	new file:   tsconfig.json 	new file:   vite.config.ts

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+</script>
+
+<div style="display: flex; justify-content: center; padding: 20px;">
+    <Button>Click me</Button>
+</div>
+
